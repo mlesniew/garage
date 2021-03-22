cab0b20ebc3da91764f52c8db9b36d2019cd48a4
Michał Leśniewski <mlesniew@gmail.com>
2021-03-22 21:31:21 +0100

    Add relays

--- src/garage.cpp
+++ src/garage.cpp
@@ -8,12 +8,31 @@ BlinkingLed red_led(D3, 0, 123, true);
 
 WiFiControl wifi_control(blue_led);
 
+template <uint8_t pin, bool inverted>
+struct Output {
+    void init(const bool initial_state = false) {
+        pinMode(pin, OUTPUT);
+        set(initial_state);
+    }
+
+    void set(bool value) {
+        digitalWrite(pin, value != inverted);
+    }
+};
+
+Output<D5, true> relay_door;
+Output<D6, true> relay_light;
+
 void setup() {
     red_led.init();
     red_led.set_pattern(0);
 
     Serial.begin(9600);
     Serial.println(F("Garage " __DATE__ " " __TIME__));
+
+    relay_door.init();
+    relay_light.init();
+
     wifi_control.init();
 
     red_led.set_pattern(0b1110);
@@ -22,4 +41,8 @@ void setup() {
 void loop() {
     wifi_control.tick();
     red_led.tick();
+
+    const auto t = millis() / 1000;
+    relay_door.set(t % 3 == 0);
+    relay_light.set(t % 5 == 0);
 }
